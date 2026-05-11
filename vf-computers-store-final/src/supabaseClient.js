aa6624620c5bec53628ec676f43d11ca5b2f3d81
viktor9804-glitch <viktor9804@gmail.com>
2026-05-11 12:06:10 +0300

    ред

--- vf-computers-store-final/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://qmuflwekhqqcfykayjdx.supabase.co";
-export const supabaseKey = "sb_publishable_GKoOE2NCrH26dUCOF5sPvg_KYgly3uc";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
